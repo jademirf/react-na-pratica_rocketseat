8c7b1bd5d97a9259cd8bdc68026c9f5cb7bc3d35
Jademir de Moura <jademirf@gmail.com>
2024-02-24 12:56:10 +0000

    add ui basic components and header

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,25 @@
+import { Plus } from "lucide-react";
+import { Header } from "./components/header";
+import { Tabs } from "./components/tabs";
 
 export function App() {
 
   return (
-    <>
-      <h1>Hi</h1>
-    </>
+    <div className="py-10 space-y-8">
+      <section>
+        <Header />
+        <Tabs />
+      </section>
+      <main className="max-w-6xl mx-auto space-y-5">
+        <div className="flex items-center gap-3">
+          <h1 className="text-xl font-bold">Tags</h1>
+          <button className="inline-flex items-center gap-1.5 text-xs bg-teal-300 text-teal-950 font-medium rounded-full px-1.5 py-1">
+            <Plus className="size-3" />
+            Create new
+          </button>
+        </div>
+      </main>
+    </div>
   )
 }
 
